23b2381f2bf55b0d97d2168d7333106bb76ac3d4
Sergey <serg.gow@gmail.com>
2024-09-26 00:46:29 +0300

    Update webgl config load with marks asset webgl adapter.

--- Repositories/ConfigRepositoryFirebaseWebGL.cs
+++ Repositories/ConfigRepositoryFirebaseWebGL.cs
@@ -1,16 +1,26 @@
 #if UNITY_WEBGL || UNITY_EDITOR
-
 using System;
-using Build1.UnityConfig.Repositories.WebGL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using Build1.UnityConfig.Utils;
+using Modules.Firebase.Adapter.Impl;
 using Newtonsoft.Json;
 using UnityEngine;
+using Rc = MarksAssets.FirebaseWebGL.RemoteConfig.RemoteConfig;
+using RemoteConfigSettings = MarksAssets.FirebaseWebGL.RemoteConfig.RemoteConfigSettings;
 
 namespace Build1.UnityConfig.Repositories
 {
     internal static class ConfigRepositoryFirebaseWebGL
     {
-        private static FirebaseRemoteConfigAgent _agent;
-        private static bool                      _fetched;
+        private static readonly Regex BooleanTruePattern  = new("^(1|true|t|yes|y|on)$", RegexOptions.IgnoreCase);
+        private static readonly Regex BooleanFalsePattern = new("^(0|false|f|no|n|off|)$", RegexOptions.IgnoreCase);
+
+        private static Rc   _remoteConfig;
+        private static bool _fetched;
 
         public static void Load<T>(ConfigSettings settings, Action<T> onComplete, Action<ConfigException> onError) where T : ConfigNode
         {
@@ -20,8 +30,7 @@ namespace Build1.UnityConfig.Repositories
                 {
                     GetJson<T>(settings, value =>
                     {
-                        var config = JsonConvert.DeserializeObject<T>((string)value);
-                        onComplete?.Invoke(config);
+                        onComplete?.Invoke(value);
                     }, onError);
                 }, onError);
             });
@@ -29,20 +38,29 @@ namespace Build1.UnityConfig.Repositories
 
         private static void Initialize(ConfigSettings settings, Action onComplete)
         {
-            if (_agent != null)
+            if (!FirebaseWebglAdapter.Initialized)
             {
-                onComplete?.Invoke();
-                return;
+                throw new Exception("FirebaseWebglAdapter isn't initialized.");
             }
 
-            var fallbackTimeout = settings.FallbackEnabled && settings.FallbackTimeout > 0 ? settings.FallbackTimeout : 0; 
-
-            _agent = new GameObject("FirebaseRemoteConfigAgent").AddComponent<FirebaseRemoteConfigAgent>();
-            _agent.OnInitialized += onComplete;
-            _agent.Initialize(Debug.isDebugBuild, fallbackTimeout);
+            SetupRemoteConfig(settings, onComplete);
         }
 
-        private static void FetchAndActivate(Action onComplete, Action<ConfigException> onError)
+        private static void SetupRemoteConfig(ConfigSettings settings, Action onComplete)
+        {
+            _remoteConfig = Rc.getRemoteConfig(FirebaseWebglAdapter.App);
+
+            var fallbackTimeout = settings.FallbackEnabled && settings.FallbackTimeout > 0 ? settings.FallbackTimeout : 0;
+
+            var minimumFetchIntervalMillis = Debug.isDebugBuild ? 0 : 300000;
+            var fetchTimeoutMillis = fallbackTimeout > 0 ? fallbackTimeout : 60000;
+
+            _remoteConfig.settings = new RemoteConfigSettings(fetchTimeoutMillis, minimumFetchIntervalMillis);
+
+            onComplete?.Invoke();
+        }
+
+        private static async void FetchAndActivate(Action onComplete, Action<ConfigException> onError)
         {
             if (_fetched)
             {
@@ -50,60 +68,148 @@ namespace Build1.UnityConfig.Repositories
                 return;
             }
 
-            _agent.OnFetchAndActivateSuccess += () =>
+            try
             {
-                _fetched = true;
-                onComplete?.Invoke();
-            };
-            _agent.OnFetchAndActivateFail += _ => { onError?.Invoke(new ConfigException(ConfigError.Unknown)); };
-            _agent.FetchAndActivate();
-        }
+                var isFetched = await Rc.fetchAndActivate(_remoteConfig);
 
-        private static void GetJson<T>(ConfigSettings settings, Action<object> onComplete, Action<ConfigException> onError)
-        {
-            switch (settings.Mode)
+                if (isFetched)
+                {
+                    _fetched = true;
+                    onComplete?.Invoke();
+                }
+                else
+                {
+                    onComplete?.Invoke();
+                }
+            }
+            catch (Exception error)
             {
-                case ConfigMode.Default:
-                    GetJsonDefault(settings, onComplete, onError);
-                    break;
-                case ConfigMode.Decomposed:
-                    GetJsonDecomposed<T>(settings, onComplete, onError);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                onError?.Invoke(new ConfigException(ConfigError.ConfigResourceNotFound, error.Message));
             }
         }
 
-        private static void GetJsonDefault(ConfigSettings settings, Action<object> onComplete, Action<ConfigException> onError)
+        private static void GetJson<T>(ConfigSettings settings, Action<T> onComplete, Action<ConfigException> onError)
         {
-            void OnGetSuccess(object value)
+            if (settings.Mode == ConfigMode.Decomposed)
             {
-                _agent.OnGetSuccess -= OnGetSuccess;
-                _agent.OnGetFail -= OnGetFail;
-                onComplete?.Invoke(value);
+                GetJsonDecomposed(settings, onComplete, onError);
+                return;
             }
 
-            void OnGetFail(FirebaseError error)
+            try
             {
-                _agent.OnGetSuccess -= OnGetSuccess;
-                _agent.OnGetFail -= OnGetFail;
-                onError?.Invoke(new ConfigException(ConfigError.Unknown));
+                var value = Rc.getValue(_remoteConfig, settings.ParameterName);
+                var config = JsonConvert.DeserializeObject<T>(value.asString());
+
+                onComplete?.Invoke(config);
             }
-            
-            _agent.OnGetSuccess += OnGetSuccess;
-            _agent.OnGetFail += OnGetFail;
-            _agent.Get(settings.ParameterName);
+            catch (Exception error)
+            {
+                onError?.Invoke(new ConfigException(ConfigError.ParsingError, error.Message));
+            }
         }
 
-        private static void GetJsonDecomposed<T>(ConfigSettings settings, Action<object> onComplete, Action<ConfigException> onError)
+        private static void GetJsonDecomposed<T>(ConfigSettings settings, Action<T> onComplete, Action<ConfigException> onError)
         {
-            // TODO: read json properties and types from provided config type
-            // TODO: request parameters one by one and assemble the composed objects
-            // TODO: serialise composed objects to json and pass it for final parsing
-            
-            throw new NotImplementedException();
+            try
+            {
+                var defaultConfig = Rc.getAll(_remoteConfig);
+
+                if (defaultConfig == null)
+                {
+                    throw new ConfigException(ConfigError.ConfigFieldNotFound, "defaultConfig is null or not found.");
+                }
+
+                var parameters = new Dictionary<string, object>();
+
+                foreach (var (key, value) in defaultConfig)
+                {
+                    var stringValue = value.asString();
+
+                    if (stringValue.Length >= 24)
+                    {
+                        parameters.Add(key, TryDecompress(stringValue));
+                    }
+                    else if (stringValue.Length > 0)
+                    {
+                        if (BooleanTruePattern.IsMatch(stringValue))
+                        {
+                            parameters.Add(key, true);
+                        }
+                        else if (BooleanFalsePattern.IsMatch(stringValue))
+                        {
+                            parameters.Add(key, false);
+                        }
+                        else if (long.TryParse(stringValue, out var valueInt))
+                        {
+                            parameters.Add(key, valueInt);
+                        }
+                        else if (double.TryParse(stringValue, out var valueDouble))
+                        {
+                            parameters.Add(key, valueDouble);
+                        }
+                        else
+                        {
+                            parameters.Add(key, stringValue);
+                        }
+                    }
+                }
+
+                var instance = ParseDecomposed<T>(parameters);
+                onComplete?.Invoke(instance);
+            }
+            catch (Exception ex)
+            {
+                onError?.Invoke(new ConfigException(ConfigError.ParsingError, ex.Message));
+            }
+        }
+
+        private static object TryDecompress(string value)
+        {
+            if (!value.StartsWith("{") || !value.EndsWith("}"))
+                value = value.Decompress();
+
+            return value;
+        }
+
+        private static T ParseDecomposed<T>(IDictionary<string, object> values)
+        {
+            var instance = Activator.CreateInstance<T>();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var jsonPropertyAttribute = property.GetCustomAttribute<JsonPropertyAttribute>();
+
+                if (jsonPropertyAttribute?.PropertyName == null)
+                    continue;
+
+                if (!values.TryGetValue(jsonPropertyAttribute.PropertyName, out var value))
+                    continue;
+
+                if (value is string json)
+                {
+                    var propertyInstance = JsonConvert.DeserializeObject(json, property.PropertyType);
+                    property.SetValue(instance, propertyInstance);
+                }
+                else
+                {
+                    property.SetValue(instance, value);
+                }
+            }
+
+            var method = instance.GetType().GetMethod("OnDeserialized", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (method == null)
+            {
+                var methods = instance.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+                method = methods.FirstOrDefault(m => m.GetCustomAttribute<OnDeserializedAttribute>() != null);
+            }
+
+            method?.Invoke(instance, new object[] { null });
+
+            return instance;
         }
     }
 }
-
 #endif
